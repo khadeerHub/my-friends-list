84829b2a6f0f3fb293f0e96ab3518ced55e76f74
Khadeer <44566841+khadeerHub@users.noreply.github.com>
2023-08-23 03:19:01 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// Array string
+var friendList = ["Hashim", "Shoaib", "Fairoz", "Rabi", "Shafiya", "Iyan"];
+// 1st Function
+function greet() {
+    var friendPicked = document.querySelector("#Friend").value.toLowerCase();
+    var friendFound = false;
+    
+    for (var i = 0; i < friendList.length; i++) {
+        if (friendList[i].toLowerCase() === friendPicked) {
+            friendFound = true;
+        }
+    }
+    
+    if (friendFound) {
+        alert("Hi, you are my friend!");
+    } else {
+        alert("You are not my friend.");
+    }
+}
+
+// 2nd function
+function addFriend() {
+    var addNewFriend = document.querySelector("#add").value;
+    friendList.push(addNewFriend); // Just push the new friend directly
+    console.log(addNewFriend + " added ✅");
+}
